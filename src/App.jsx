a3a885e344f2635ac4770247607f960d0352c3a3
LudvigP <75888209+HIIMSKYDAZ@users.noreply.github.com>
2024-12-02 11:24:44 +0100

    darts

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,6 @@ import {DartsListPage} from "./DartsListPage";
 import { DartsCreatePage } from "./DartsCreatePage";
 import { DartsModPage } from "./DartsModPage";
 import { DartsDelPage } from "./DartsDelPage";
-import { ChessListPage } from "./ChessListPage";
-import { ChessSinglePage } from "./ChessSinglePage";
-import { ChessCreatePage } from "./ChessCreatePage";
-import { ChessModPage } from "./ChessModPage";
-import { ChessDelPage } from "./ChessDelPage";
 import './App.css';
 
 export const App =()=> {
@@ -16,16 +11,6 @@ export const App =()=> {
       <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
-            <li className="nav-item">
-              <NavLink to={'/sakkozok'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
-                <span className="nav-link">Sakkozók</span>
-              </NavLink>
-              </li>
-              <li className="nav-item">
-              <NavLink to={'/uj-chess'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
-                <span className="nav-link">Új sakkozó felvitele</span>
-              </NavLink>
-              </li>
               <li className="nav-item">
               <NavLink to={'/'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
                 <span className="nav-link">Darts</span>
@@ -41,9 +26,6 @@ export const App =()=> {
       </nav>
       <Routes>
         <Route path="/" exact element={<DartsListPage />} />
-        <Route path="/sakkozok" exact element={<ChessListPage />} />
-        <Route path="/chess/:chessId" exact element={<ChessSinglePage />} />
-        <Route path="/uj-chess" exact element={<ChessCreatePage />} />
         <Route path="/uj-darts" exact element={<DartsCreatePage />} />
         <Route path="/mod-chess/:chessId" exact element={<ChessModPage />} />
         <Route path="/mod-darts/:dartsId" exact element={<DartsModPage />} />
