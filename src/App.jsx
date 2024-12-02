bbbc248a3d9ae2a113020eacc007fd9fa701c29f
LudvigP <75888209+HIIMSKYDAZ@users.noreply.github.com>
2024-12-02 09:12:23 +0100

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,8 @@
 import { BrowserRouter as Router, NavLink, Routes, Route } from "react-router-dom";
+import {DartsListPage} from "./DartsListPage";
+import { DartsCreatePage } from "./DartsCreatePage";
+import { DartsModPage } from "./DartsModPage";
+import { DartsDelPage } from "./DartsDelPage";
 import { ChessListPage } from "./ChessListPage";
 import { ChessSinglePage } from "./ChessSinglePage";
 import { ChessCreatePage } from "./ChessCreatePage";
@@ -13,7 +17,7 @@ export const App =()=> {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav">
             <li className="nav-item">
-              <NavLink to={'/'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
+              <NavLink to={'/sakkozok'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
                 <span className="nav-link">Sakkozók</span>
               </NavLink>
               </li>
@@ -22,15 +26,29 @@ export const App =()=> {
                 <span className="nav-link">Új sakkozó felvitele</span>
               </NavLink>
               </li>
+              <li className="nav-item">
+              <NavLink to={'/'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
+                <span className="nav-link">Darts</span>
+              </NavLink>
+              </li>
+              <li className="nav-item">
+              <NavLink to={'/uj-darts'} className={({isActive}) => "nav-link" + (isActive ? "active" : "")}>
+                <span className="nav-link">Új Dartsozó felvitele</span>
+              </NavLink>
+              </li>
           </ul>
         </div>
       </nav>
       <Routes>
-        <Route path="/" exact element={<ChessListPage />} />
+        <Route path="/" exact element={<DartsListPage />} />
+        <Route path="/sakkozok" exact element={<ChessListPage />} />
         <Route path="/chess/:chessId" exact element={<ChessSinglePage />} />
         <Route path="/uj-chess" exact element={<ChessCreatePage />} />
+        <Route path="/uj-darts" exact element={<DartsCreatePage />} />
         <Route path="/mod-chess/:chessId" exact element={<ChessModPage />} />
+        <Route path="/mod-darts/:dartsId" exact element={<DartsModPage />} />
         <Route path="/del-chess/:chessId" exact element={<ChessDelPage />} />
+        <Route path="/del-darts/:dartsId" exact element={<DartsDelPage />} />
       </Routes>
     </Router>
   );
